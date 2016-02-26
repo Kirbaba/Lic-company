0bdf5f9ffefef4547878395c495b38007c36f2b7
Vladislav <v.kirbaba@gmail.com>
2016-02-26 12:55:07 +0300

    Корзина

--- page-basket.php
+++ page-basket.php
@@ -9,6 +9,80 @@
 			<!-- close .block_title -->
 		</div>
 		<!-- close .basket--head -->
+		<!-- open .contain -->
+		<div class="contain">
+			<!-- open .basket__box -->
+			<div class="basket__box">
+				<!-- open .basket__box--head -->
+				<div class="basket__box--head">
+					<p class="basket__box--info--name">Наименование</p>
+					<p class="basket__box--info--count">Кол-во  </p>
+					<p class="basket__box--info--price">Стоимость</p>
+					<p class="basket__box--info--sum">Сумма</p>
+				</div>
+				<!-- close .basket__box--head -->
+				
+				<!-- open .basket__box--line -->
+				<div class="basket__box--line">
+					<!-- open .basket__box--thumb -->
+					<div class="basket__box--thumb"></div>
+					<!-- close .basket__box--thumb -->
+					<!-- open .basket__box--info -->
+					<div class="basket__box--info">
+						<!-- open .basket__box--info--name -->
+						<div class="basket__box--info--name"></div>
+						<!-- close .basket__box--info--name -->
+						<!-- open .basket__box--info--count -->
+						<div class="basket__box--info--count"></div>
+						<!-- close .basket__box--info--count -->
+						<!-- open .basket__box--info--price -->
+						<div class="basket__box--info--price"></div>
+						<!-- close .basket__box--info--price -->
+						<!-- open .basket__box--info--sum -->
+						<div class="basket__box--info--sum"></div>
+						<!-- close .basket__box--info--sum -->
+					</div>
+					<!-- close .basket__box--info -->
+					<a href="#" class="basket__box--del-but">удалить</a>
+				</div>
+				<!-- close .basket__box--line -->
+
+				<!-- open .basket__box--line -->
+				<div class="basket__box--line">
+					<!-- open .basket__box--thumb -->
+					<div class="basket__box--thumb"></div>
+					<!-- close .basket__box--thumb -->
+					<!-- open .basket__box--info -->
+					<div class="basket__box--info">
+						<!-- open .basket__box--info--name -->
+						<div class="basket__box--info--name"></div>
+						<!-- close .basket__box--info--name -->
+						<!-- open .basket__box--info--count -->
+						<div class="basket__box--info--count"></div>
+						<!-- close .basket__box--info--count -->
+						<!-- open .basket__box--info--price -->
+						<div class="basket__box--info--price"></div>
+						<!-- close .basket__box--info--price -->
+						<!-- open .basket__box--info--sum -->
+						<div class="basket__box--info--sum"></div>
+						<!-- close .basket__box--info--sum -->
+					</div>
+					<!-- close .basket__box--info -->
+					<a href="#" class="basket__box--del-but">удалить</a>
+				</div>
+				<!-- close .basket__box--line -->
+
+				<!-- open .basket__box--total -->
+				<div class="basket__box--total">
+					<p>Итого:</p>
+					<p></p>
+					<a href="#" class="basket__box--total--order">оформить заказ</a>
+				</div>
+				<!-- close .basket__box--total -->
+			</div>
+			<!-- close .basket__box -->
+		</div>
+		<!-- close .contain -->
 	</div>
 	<!-- close .page-box -->
 <?php get_footer(); ?>
