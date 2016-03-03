8f489ee8571dda8c618c024a5d99a22a0a7024ea
Vladislav <v.kirbaba@gmail.com>
2016-03-03 15:42:44 +0300

    Регистрация

---
+++ page-registration.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+	<!-- open .page-box -->
+	<div class="page-box">
+		<!-- open .basket--head -->
+		<div class="basket--head">
+			<!-- open .block_title -->
+			<h2 class="block_title"><span>Вход</span></h2>
+			<!-- close .block_title -->
+		</div>
+		<!-- open .contain -->
+		<div class="contain">	
+			<!-- open .enter -->
+			<div class="enter">
+				<input type="text" name="registration--name" class="enter--inp" placeholder="ФИО" />
+				<input type="tel" name="registration--pgone" class="enter--inp" placeholder="Телефон" />
+				<input type="email" name="registration--mail" class="enter--inp" placeholder="E-mail " />
+				<input type="password" name="registration--pas" class="enter--inp" placeholder="Пароль " />
+				
+				<a href="#" class="registration--but">зарегистрироваться</a>
+			</div>
+			<!-- close .enter -->
+		</div>
+		<!-- close .contain -->
+	</div>
+	<!-- close .page-box -->
+
+
+<?php get_footer(); ?>
