7aa80316111fd8b889220b41d77bbe2ef475383e
Vladislav <v.kirbaba@gmail.com>
2016-03-03 15:34:00 +0300

    Вход

---
+++ page-enter.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+	<!-- open .page-box -->
+	<div class="page-box">
+		<!-- open .basket--head -->
+		<div class="basket--head">
+			<!-- open .block_title -->
+			<h2 class="block_title"><span>Вход</span></h2>
+			<!-- close .block_title -->
+		</div>
+		<!-- open .contain -->
+		<div class="contain">	
+			<!-- open .enter -->
+			<div class="enter">
+				<input type="text" name="enter--email" class="enter--inp" placeholder="Введите E-mail " />
+				<input type="text" name="enter--password" class="enter--inp" placeholder="Введите пароль "/>
+				<a href="#" class="enter--but">войти</a>
+			</div>
+			<!-- close .enter -->
+		</div>
+		<!-- close .contain -->
+	</div>
+	<!-- close .page-box -->
+
+
+<?php get_footer(); ?>
